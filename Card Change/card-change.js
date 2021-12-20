cd56c6695739c43d40a62c47447be6d0a203a26a
MAM <70688333+Muzammil538@users.noreply.github.com>
2021-12-20 18:58:09 +0530

    Added

---
+++ Card Change/card-change.js
@@ -0,0 +1,13 @@
+const CardCarol = document.querySelector(".card-carol")
+const Next = document.querySelector("#next");
+const Pre = document.querySelector("#pre");
+// console.log(CardCarol.children[1]);
+
+function MoveNext () {
+    for(let i=0; i<CardCarol.children.length; i++){
+        // console.log(CardCarol.children[i]);
+        if(CardCarol.children[i].classList == "card fade"){
+            
+        }
+    }
+}
