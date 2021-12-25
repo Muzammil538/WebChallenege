345a06dd427a8b976cfa1c09917bdbccd37bb0d6
MAM <70688333+Muzammil538@users.noreply.github.com>
2021-12-25 16:46:32 +0530

    bnner

--- cwmam_react/src/Components/Main/Banner/banner.js
+++ cwmam_react/src/Components/Main/Banner/banner.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './banner.css';
-import Image from "../../../Media/banner1.png"
+import Image1 from "../../../Media/banner1.png"
 
 export default function banner() {
+    window.addEventListener("load", ()=>{
+        document.querySelector("#Image1").classList.add("left");
+        document.querySelector(".banner_span1").classList.add("right");
+        document.querySelector(".banner_span2").classList.add("right");
+        document.querySelector(".banner_span3").classList.add("right");
+    })
     return (
         <>
         <div className="banner">
             <div className="left-side">
-                <h2>This is CodeWithMAM</h2>
+                <h2 className="banner_text">This is <span className="banner_span1">Code</span><span className="banner_span2">With</span><span className="banner_span3">MAM</span></h2>
             </div>
             <div className="right-side">
-                <img src={Image} alt="Imge" />
+                <img src={Image1} alt="Imge" id="Image1"/>
             </div>
         </div>
         </>
