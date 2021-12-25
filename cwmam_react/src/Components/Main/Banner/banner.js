2bd9efaa3ad5d7a9e8f41eb4db9081f17c0df8af
MAM <70688333+Muzammil538@users.noreply.github.com>
2021-12-25 14:07:55 +0530

    dded

--- cwmam_react/src/Components/Main/Banner/banner.js
+++ cwmam_react/src/Components/Main/Banner/banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './banner.css';
+import "../../../../public/Media/banner1.png";
 
 export default function banner() {
     return (
@@ -8,7 +9,9 @@ export default function banner() {
             <div className="left-side">
                 <h2>This is CodeWithMAM</h2>
             </div>
-            <div className="right-side"></div>
+            <div className="right-side">
+                <img src="../../../../public/Media/banner1.png" alt="Imge" />
+            </div>
         </div>
         </>
     )
