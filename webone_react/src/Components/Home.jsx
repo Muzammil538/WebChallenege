986e0ef539d27e6155251aed8d45a968fb98acee
MAM <70688333+Muzammil538@users.noreply.github.com>
2021-12-31 16:27:41 +0530

    add

--- webone_react/src/Components/Home.jsx
+++ webone_react/src/Components/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "./Home.css";
 export default function Home() {
     return (
         <>
             <nav>
-                <ul>
-                    <li> <Link to="./">Home</Link></li>
+            <li className="t-Logo"><Link to="./" >WEBTEACH</Link></li>
+                <ul className="Links">
+                    <li><Link to="./">Services</Link></li>
+                    <li><Link to="./">Courses</Link></li>
+                    <li><Link to="./">About</Link></li>
+                    <li><Link to="./">Contact</Link></li>
                 </ul>
-            </nav> 
+            </nav>
+            
         </>
     )
 }
